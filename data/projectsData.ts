5bf6d5f8a17ccd6b32d65a49b5b0f61be82e6db2
jboliv01 <57547273+jboliv01@users.noreply.github.com>
2024-02-26 14:59:48 -0500

    update project data to include week 4

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -6,11 +6,12 @@ interface Project {
 }
 
 const projectsData: Project[] = [
+ 
   {
-    title: 'Coming Soon',
-    description: `Projects coming soon!`,
-    imgSrc: '/static/images/arch_2.png',
-    href: 'https://www.google.com',
+    title: 'End-to-End Data Engineering with dbt and Looker Studio',
+    description: `This project showcases an end-to-end data engineering workflow, starting from data extraction from external sources to storage in Google Cloud Storage. It involves creating a robust dbt model for data transformation and concludes with data visualization using Looker Studio. The project demonstrates practical application of modern data engineering techniques and provides insights into effective data pipeline creation, management, and business intelligence analysis.`,
+    imgSrc: '/static/images/arch_2.png', // Replace with the path to your image
+    href: 'https://www.jonahboliver.com/blog/de-zc-w4', 
   },
 ]
 
