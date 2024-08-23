99cfa377e14ebaf24e2c7be736517405515f6f7f
Jonah Oliver <jboliv01@gmail.com>
2024-08-23 16:51:58 -0400

    add project image nav

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,56 +1,86 @@
-import Image from './Image'
-import Link from './Link'
+'use client';
 
-const Card = ({ title, description, imgSrc, href }) => (
-  <div className="md max-w-[544px] p-4 md:w-1/2">
-    <div
-      className={`${
-        imgSrc && 'h-full'
-      }  overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 dark:border-gray-700`}
-    >
-      {imgSrc &&
-        (href ? (
-          <Link href={href} aria-label={`Link to ${title}`}>
-            <Image
-              alt={title}
-              src={imgSrc}
-              className="object-cover object-center md:h-36 lg:h-48"
-              width={544}
-              height={306}
-            />
-          </Link>
-        ) : (
-          <Image
-            alt={title}
-            src={imgSrc}
-            className="object-cover object-center md:h-36 lg:h-48"
-            width={544}
-            height={306}
-          />
-        ))}
-      <div className="p-6">
-        <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
-          {href ? (
-            <Link href={href} aria-label={`Link to ${title}`}>
-              {title}
-            </Link>
-          ) : (
-            title
-          )}
-        </h2>
-        <p className="prose mb-3 max-w-none text-gray-500 dark:text-gray-400">{description}</p>
-        {href && (
-          <Link
-            href={href}
-            className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-            aria-label={`Link to ${title}`}
-          >
-            Learn more &rarr;
-          </Link>
+import { useState } from 'react';
+import Image from './Image';
+import Link from './Link';
+
+const Card = ({ title, description, imgSrcArray = [], href }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrevImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + imgSrcArray.length) % imgSrcArray.length);
+  };
+
+  const handleNextImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % imgSrcArray.length);
+  };
+
+  return (
+    <div className="md:max-w-[544px] p-4 md:w-1/2">
+      <div
+        className={`${
+          imgSrcArray && 'h-full'
+        } overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 dark:border-gray-700`}
+      >
+        {imgSrcArray && imgSrcArray.length > 0 && (
+          <div className="relative">
+            {href ? (
+              <Link href={href} aria-label={`Link to ${title}`}>
+                <Image
+                  alt={title}
+                  src={imgSrcArray[currentIndex]}
+                  className="object-cover object-center md:h-36 lg:h-48 w-full" // Ensure full width and cover
+                  width={544} // These dimensions help in maintaining the aspect ratio
+                  height={306}
+                />
+              </Link>
+            ) : (
+              <Image
+                alt={title}
+                src={imgSrcArray[currentIndex]}
+                className="object-cover object-center md:h-36 lg:h-48 w-full" // Ensure full width and cover
+                width={544}
+                height={306}
+              />
+            )}
+            <button
+              className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-gray-800 bg-opacity-50 text-white p-2 rounded-r-md"
+              onClick={handlePrevImage}
+            >
+              &#10094;
+            </button>
+            <button
+              className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-gray-800 bg-opacity-50 text-white p-2 rounded-l-md"
+              onClick={handleNextImage}
+            >
+              &#10095;
+            </button>
+          </div>
         )}
+        <div className="p-6">
+          <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
+            {href ? (
+              <Link href={href} aria-label={`Link to ${title}`}>
+                {title}
+              </Link>
+            ) : (
+              title
+            )}
+          </h2>
+          <p className="prose mb-3 max-w-none text-gray-500 dark:text-gray-400">{description}</p>
+          {href && (
+            <Link
+              href={href}
+              className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+              aria-label={`Link to ${title}`}
+            >
+              Learn more &rarr;
+            </Link>
+          )}
+        </div>
       </div>
     </div>
-  </div>
-)
+  );
+};
 
-export default Card
+export default Card;
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -5,20 +5,29 @@ interface Project {
   imgSrc?: string,
 }
 
-const projectsData: Project[] = [
- 
+const projectsData = [
   {
     title: 'End-to-End Data Engineering with dbt and Looker Studio',
-    description: `This project showcases an end-to-end data engineering workflow, starting from data extraction from external sources to storage in Google Cloud Storage. It involves creating a robust dbt model for data transformation and concludes with data visualization using Looker Studio. The project demonstrates practical application of modern data engineering techniques and provides insights into effective data pipeline creation, management, and business intelligence analysis.`,
-    imgSrc: '/static/images/arch_2.png', // Replace with the path to your image
+    description: `This project showcases an end-to-end data engineering workflow...`,
+    imgSrcArray: [
+      '/static/images/de-zc/w4/dashboard.png',
+      '/static/images/de-zc/w4/dbt-dag.png',
+      '/static/images/de-zc/w4/dbt-staging-dag.png',
+      
+      
+    ],
     href: 'https://www.jonahboliver.com/blog/de-zc-w4', 
   },
   {
     title: 'End-to-End Data Engineering with Dagster, AWS S3, Apache Spark and dbt',
-    description: `This project aims to implement a scalable end-to-end solution that efficiently processes approximately 8GB of data using advanced ETL techniques and big data technologies, ensuring that insights are both accessible and actionable. It involves creating a robust dbt model for data transformation and concludes with data visualization using Looker Studio. The project demonstrates practical application of modern data engineering techniques and provides insights into effective data pipeline creation, management, and business intelligence analysis.`,
-    imgSrc: '/static/images/de-zc-capstone-dag.png', // Replace with the path to your image
-    href: 'https://github.com/jboliv01/yelp_end_to_end_batch_pipeline?tab=readme-ov-file#yelp-data-insights-dashboard', 
+    description: `This project aims to implement a scalable end-to-end solution...`,
+    imgSrcArray: [
+      '/static/images/de-zc-capstone-dag.png',
+      '/static/images/de-zc/w5/spark-job-complete.png'
+      
+    ],
+    href: 'https://github.com/jboliv01/yelp_end_to_end_batch_pipeline?tab=readme-ov-file#yelp-data-insights-dashboard',
   },
-]
+];
 
-export default projectsData
+export default projectsData;
